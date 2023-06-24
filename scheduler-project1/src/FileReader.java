d1e9bbb80d2ab85fa6cdbcbfc5155a8a17fe63bf
SajedHamdan09 <132995155+SajedHamdan09@users.noreply.github.com>
2023-06-24 12:54:41 +0300

    Update FileReader.java

--- scheduler-project1/src/FileReader.java
+++ scheduler-project1/src/FileReader.java
@@ -1,6 +1,12 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.UUID;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -12,8 +18,8 @@ public class FileReader {
         this.file = file;
     }
 
-    public LinkedList<course> readCoursesFromSheet(String sheetName) throws IOException {
-        LinkedList<course> courseList = new LinkedList<>();
+    public Map<UUID, Course> readCoursesFromSheet(String sheetName) throws IOException {
+       HashMap<UUID, Course> Courses = new HashMap<UUID, Course>();
 
         FileInputStream fis = new FileInputStream(file);
         Workbook workbook = new XSSFWorkbook(fis);
@@ -35,7 +41,7 @@ public class FileReader {
                 String course_type = "";
                 String num_of_slots = "";
                 LinkedList<String>conflicting_courses;
-                LinkedList<String>instructors_day;
+                String instructors_day;
 
                 // Iterate over cells in the row
                 for (Cell cell : row) {
@@ -76,22 +82,133 @@ public class FileReader {
                             conflicting_courses = cell.getStringCellValue();
                             break; 
                         case 13:
-                            instructors_day = new LinkedList<String>();
                             instructors_day = cell.getStringCellValue();
                             break;                                         
                     }
                 }
 
-                course course = new course(course_id, course_name, Integer.parseInt(num_credits),Integer.parseInt(num_sections),Integer.parseInt(num_sessions),instructor_name,
-                                        instructors_day,instructor_hours,conflicting_courses,course_type, Integer.parseInt(num_of_slots));
-                courseList.add(course);
+               LinkedList<String>Split_Days = Split_Days(instructors_day);
+               LinkedList<Integer>instructor_days = new LinkedList<Integer>();
+
+               for(int i=0;i<Split_Days.size();i++){
+                   instructor_days.add(getDayIndex(Split_Days.get(i)));
+               }
+
+              
+              LinkedList<String> timeslots = convertHourstoSlots(instructor_hours);
+               int[] slots = getSlotsIndicies(timeslots.getFirst() , timeslots.getLast());
+               int index1 = slots[0];
+               int index2 = slots[1];
+
+
+               Course course = new  Course(course_id, course_name, Integer.parseInt(num_credits),Integer.parseInt(num_sections),Integer.parseInt(num_sessions), instructor_name,
+                                instructor_days, index1, index2, conflicting_courses , course_type ,Integer.parseInt(num_of_slots) );
+
+            //    Course course = new Course(course_id, course_name, Integer.parseInt(num_credits),Integer.parseInt(num_sections),Integer.parseInt(num_sessions),instructor_name,
+             //                           instructors_day,instructor_hours,conflicting_courses,course_type, Integer.parseInt(num_of_slots));
+                Courses.put(UUID.randomUUID() , course);
             }
         } 
 
         workbook.close();
         fis.close();
 
-        return courseList;
+        return Courses;
     }
 
-}
+     private int getDayIndex(String day) {
+        switch (day) {
+            case "Monday":
+                return 0;
+            case "Tuesday":
+                return 1;
+            case "Wednesday":
+                return 2;
+            case "Thursday":
+                return 3;
+            case "Friday":
+                return 4;
+            default:
+                throw new IllegalArgumentException("Invalid day: " + day);
+        }
+    }
+
+     private LinkedList<String> convertHourstoSlots(String instructorHours){
+        LinkedList<String> slots = new LinkedList<>();
+        String[] hours = instructorHours.split("/");
+
+        for (String hour : hours) {
+            slots.add(hour.trim());
+        }
+
+        return slots;
+    }
+
+    private LinkedList<String> Split_Days(String instructors_day){
+        LinkedList<String> slots = new LinkedList<String>();
+        String[] hours = instructors_day.split("and");
+
+        for (String hour : hours) {
+            slots.add(hour.trim());
+        }
+
+        return slots;
+    }
+
+
+      private int[] getSlotsIndicies(String hour1, String hour2) {
+        LocalTime startTime = LocalTime.parse(hour1);
+        LocalTime endTime = LocalTime.parse(hour2);
+
+
+        LocalTime[] slots = {
+            LocalTime.parse("08:00"), // Slot 0
+            LocalTime.parse("09:30"), // Slot 1
+            LocalTime.parse("11:00"), // Slot 2
+            LocalTime.parse("13:00"), // Slot 3
+            LocalTime.parse("14:30"), // Slot 4
+            LocalTime.parse("16:00"), // Slot 5
+            LocalTime.parse("17:15")  // Final hour
+        };
+
+        int startSlot = -1;
+        int endSlot = -1;
+
+        // Find the first slot
+        if((startTime.isAfter(LocalTime.parse("12:15")) && startTime.isBefore(LocalTime.parse("13:00"))) // between 12:15 and 1 exclusive
+            || startTime.equals(LocalTime.parse("12:15"))){
+                startSlot = 3;
+        }else{
+            boolean slotFound = false;
+            for (int i = 0; i < slots.length; i++) {
+                if (startTime.isBefore(slots[i]) || startTime.equals(slots[i])) {
+                    startSlot = i;
+                    slotFound = true;
+                    break;
+                }
+            }
+            if(!slotFound){
+                startSlot = slots.length-2;
+            }
+        }
+// Find the last slot
+        if((endTime.isAfter(LocalTime.parse("12:15")) && endTime.isBefore(LocalTime.parse("13:00"))) // between 12:15 and 1 exclusive
+            || endTime.equals(LocalTime.parse("12:15"))){
+            endSlot = 3;
+        }else{
+            boolean slotFound = false;
+            for (int i = startSlot; i < slots.length; i++) {
+                if (endTime.isBefore(slots[i]) || endTime.equals(slots[i])) {
+                    endSlot = i-1;
+                    slotFound = true;
+                    break;
+                }
+                if(!slotFound){
+                    endSlot = slots.length-1;
+                }
+            }
+        }
+        // Return the viable slots as an array
+        return new int[]{startSlot, endSlot};
+    }
+ }
